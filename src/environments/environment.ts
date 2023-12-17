a6f04492124e0df235671b81a904291d18dff569
Vũ Mạnh Tuấn <manhtuanst04@gmail.com>
2023-12-17 22:36:32 +0700

    first commit

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyDMPiGOdDJKIXPpIFJGenj17GlB7JTyGbo",
+        authDomain: "wedding-test-86755.firebaseapp.com",
+        projectId: "wedding-test-86755",
+        storageBucket: "wedding-test-86755.appspot.com",
+        messagingSenderId: "729748154263",
+        appId: "1:729748154263:web:56fb5ddca106a1b8fa06d2",
+        measurementId: "G-HRB1FT6E9S"
+      }
+  };
