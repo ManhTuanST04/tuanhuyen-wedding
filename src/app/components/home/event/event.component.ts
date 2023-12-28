c3b386eaba25f5ca3624fe9e139970f820c37aee
Vũ Mạnh Tuấn <manhtuanst04@gmail.com>
2023-12-28 22:26:16 +0700

    update event

--- src/app/components/home/event/event.component.ts
+++ src/app/components/home/event/event.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-event',
-  standalone: true,
-  imports: [],
-  templateUrl: './event.component.html',
-  styleUrl: './event.component.css'
+    selector: 'app-event',
+    standalone: true,
+    imports: [],
+    templateUrl: './event.component.html',
+    styleUrl: './event.component.css'
 })
 export class EventComponent {
+    onOpenMap(gender: string): void {
+        if (!gender) return;
 
+        let mapLink = '';
+        if (gender === 'male') {
+            mapLink = 'https://maps.app.goo.gl/Bt6NfSEqjiQGjywYA';
+        } else {
+            mapLink = 'https://maps.app.goo.gl/Ga5wz15S43QsBcAW8';
+        }
+
+        window.open(mapLink);
+    }
 }
