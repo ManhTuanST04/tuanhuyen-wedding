960080a96dbb21ac06bdebbf3aa9cee8df96d7f7
Vũ Manh Tuan <manhtuanst04gmail.com>
2024-02-21 23:16:22 +0700

    add production config

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -10,13 +10,13 @@ export const GG_MAP_MALE = 'https://maps.app.goo.gl/Bt6NfSEqjiQGjywYA';
 export const GG_MAP_FEMALE = 'https://maps.app.goo.gl/Ga5wz15S43QsBcAW8';
 
 export const EVENT_INFO_MALE = {
-    START_TIME: '20240105T224500',
+    START_TIME: '20240324T110000',
     END_TIME: '20240105T231500',
     LOCATION: 'Khu 11 - Hiền Lương - Hạ Hòa - Phú Thọ'
 };
 
 export const EVENT_INFO_FEMALE = {
-    START_TIME: '20240105T224500',
+    START_TIME: '20240317T110000',
     END_TIME: '20240105T231500',
     LOCATION: 'Tùng Châu - Đức Thọ - Hà Tĩnh'
 };
