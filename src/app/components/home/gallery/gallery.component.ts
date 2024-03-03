38bccdb95bff97e86925827c77d3aba1905ea819
Vũ Manh Tuan <manhtuanst04gmail.com>
2024-03-03 18:08:36 +0700

    update slide album

--- src/app/components/home/gallery/gallery.component.ts
+++ src/app/components/home/gallery/gallery.component.ts
@@ -1,12 +1,76 @@
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { forEach } from 'lodash';
+import { CarouselModule, OwlOptions } from 'ngx-owl-carousel-o';
+import { throwError } from 'rxjs';
+
+export interface PhotosApi {
+    albumId?: number;
+    id?: number;
+    title?: string;
+    url?: string;
+    thumbnailUrl?: string;
+}
 
 @Component({
-  selector: 'app-gallery',
-  standalone: true,
-  imports: [],
-  templateUrl: './gallery.component.html',
-  styleUrl: './gallery.component.css'
+    selector: 'app-gallery',
+    standalone: true,
+    imports: [CommonModule, CarouselModule],
+    templateUrl: './gallery.component.html',
+    styleUrl: './gallery.component.css'
 })
 export class GalleryComponent {
+    maxImageItem = 19;
+    urlBaseAlbum = 'assets/img/tuanhuyen/album';
+    albumData?: any = [];
 
+    ngOnInit() {
+        this.getDataImages();
+    }
+
+    getDataImages() {
+        for (let i = 0; i < this.maxImageItem; i++) {
+            let item: PhotosApi = {
+                albumId: 1,
+                id: i++,
+                title: 'Ảnh cưới Tuấn Huyền',
+                url: `${this.urlBaseAlbum}/${i++}.jpg`,
+                thumbnailUrl: `${this.urlBaseAlbum}/${i++}.jpg`
+            };
+
+            this.albumData?.push(item);
+        }
+    }
+
+    customOptions: OwlOptions = {
+        nav: false,
+        navText: [
+            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+        ],
+        loop: true,
+        autoplay: true,
+        center: true,
+        dots: false,
+        autoHeight: true,
+        autoWidth: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            576: {
+                items: 2
+            },
+            768: {
+                items: 3
+            },
+            992: {
+                items: 4
+            },
+            1200: {
+                items: 5
+            }
+        }
+    };
 }
