8ae46e621a16dab188815320b9b2386b2c495e46
Vũ Mạnh Tuấn <35447068+ManhTuanST04@users.noreply.github.com>
2024-03-07 10:05:45 +0000

    update env

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
     production: false,
     firebase: {
-        apiKey: "AIzaSyDMPiGOdDJKIXPpIFJGenj17GlB7JTyGbo",
-        authDomain: "wedding-test-86755.firebaseapp.com",
-        projectId: "wedding-test-86755",
-        storageBucket: "wedding-test-86755.appspot.com",
-        messagingSenderId: "729748154263",
-        appId: "1:729748154263:web:56fb5ddca106a1b8fa06d2",
-        measurementId: "G-HRB1FT6E9S"
+        apiKey: "AIzaSyBOcyypzn8vyDW6Mm4-eWlgPnMPeL9KGHM",
+        authDomain: "tuanhuyen-wedding.firebaseapp.com",
+        projectId: "tuanhuyen-wedding",
+        storageBucket: "tuanhuyen-wedding.appspot.com",
+        messagingSenderId: "871811499542",
+        appId: "1:871811499542:web:30f26956749c491b299737",
+        measurementId: "G-QYESXZEKM8"
       }
   };
